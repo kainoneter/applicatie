bde73bcdc2554894513d66409c4c306f998a7aa2
kainoneter <kainscheffer2005@gmail.com>
2025-09-30 15:40:59 +0200

    api w test route

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function() {
+
+});
+
+Route::any('/test', function() {
+    return response()->json('hoi');
+});
