f609140443448fb2d348596b70b0e42293bc7c62
kainoneter <kainscheffer2005@gmail.com>
2025-10-24 15:01:49 +0200

    test

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -3,15 +3,15 @@ import en from './locales/en.json'
 import nl from './locales/nl.json'
 
 const locale = navigator.language.split('-')[0];
-const messages = {
-  en: { hello: 'Hello' },
-  nl: { hello: 'Bonjour' }
-}
+
 const i18n = createI18n({
     legacy: false,
     locale: locale,
     fallbackLocale: 'en',
-    messages,
+    messages: {
+        en,
+        nl
+    },
 });
 
 
